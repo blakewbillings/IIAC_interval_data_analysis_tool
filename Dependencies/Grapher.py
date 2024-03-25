6c0918781417c75953f4a26d06b382b5c90b36d4
jFlyn <jFlyn@LAPTOPLAPTOP>
2024-03-25 16:57:08 -0600

    Added Comment (Tes)

--- Dependencies/Grapher.py
+++ Dependencies/Grapher.py
@@ -25,7 +25,7 @@ class Grapher:
         #a list of hours of the day so we can label the x ticks
         self.Ip = ['12 am', '1 am', '2 am', '3 am', '4 am', '5 am', '6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm', '8 pm', '9 pm', '10 pm', '11 pm', '12 am', '']
 
-        #a way to add commas to axis values in the thousands
+        #a way to add commas to axis values in the thousands 
         self.commas = ticker.StrMethodFormatter('{x:,.0f}')
 
     def graphAll(self):
